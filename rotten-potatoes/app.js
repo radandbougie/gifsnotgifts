3550fee9de5d2da6af63b0ca0181ed98816efe54
Kandy M <studentloaner11@Thazins-MacBook-Air.local>
2018-09-11 19:59:39 -0700

    Users can create reviews

--- rotten-potatoes/app.js
+++ rotten-potatoes/app.js
@@ -3,6 +3,24 @@ app.get('/reviews', (req, res) => {
   res.render('reviews-index', { reviews: reviews });
 })
 
+app.use(bodyParser.urlencoded({ extended: true }));
+// CREATE
+app.post('/reviews', (req, res) => {
+  console.log(req.body);
+  // res.render('reviews-new', {});
+})
+
+// app.js
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 // app.js
 
 // OUR MOCK ARRAY OF PROJECTS
@@ -34,6 +52,8 @@ app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
 // app.js
 
 // INDEX
@@ -75,4 +95,12 @@ app.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
     })
+})
+
+
+// app.js
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
 })
